d2e28f1c651cc106849ac0994fdc20c05638a5e1
Ariane Lozac'hmeur <ariane.lozachmeur@gmail.com>
2018-08-15 22:58:44 -0500

    Header in place

--- resources/js/init.js
+++ resources/js/init.js
@@ -1,8 +1,15 @@
 (function($){
   $(function(){
 
+  $(document).ready(function(){
     $('.sidenav').sidenav();
-    $('.parallax').parallax();
+  });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, {indicators:false, height:500});
+  });
